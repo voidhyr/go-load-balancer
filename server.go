ce9ee1fbfadbd0939e6dcbe9f38665186efd43ae
voidhyr <samdani295@gmail.com>
2026-04-09 20:56:01 +0530

    feat: add backend HTTP server with configurable port

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Args[1] // pass port from terminal
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from server on port %s\n", port)
+	})
+
+	fmt.Printf("Server running on port %s\n", port)
+	http.ListenAndServe(":"+port, nil)
+}
